fa4c2ddbf57cf5f21a5dee17a7b407cae39fbde1
Jeff Stieler <jeff.m.stieler@gmail.com>
2013-06-23 18:16:51 -0400

    Switch private properties to protected for subclassing.  Move DOMDocument processing to named function so constructor can be overridden.

--- SimpleScraper.class.php
+++ SimpleScraper.class.php
@@ -16,14 +16,14 @@
 */
 
 class SimpleScraper {
-	
-	private
+
+	protected
 		$contentType,
 		$data,
 		$content,
 		$httpCode,
 		$url;
-	
+
 /*===========================================================================*/
 // CONSTRUCTOR
 /*===========================================================================*/
@@ -38,15 +38,106 @@ class SimpleScraper {
 			'twitter' => array(),
 			'meta' => array()
 		);
-		
+
 		$urlPattern = '/\(?\b(?:(http|https|ftp):\/\/)?((?:www.)?[a-zA-Z0-9\-\.]+[\.][a-zA-Z]{2,4}|localhost(?=\/)|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::(\d*))?(?=[\s\/,\.\)])([\/]{1}[^\s\?]*[\/]{1})*(?:\/?([^\s\n\?\[\]\{\}\#]*(?:(?=\.)){1}|[^\s\n\?\[\]\{\}\.\#]*)?([\.]{1}[^\s\?\#]*)?)?(?:\?{1}([^\s\n\#\[\]\(\)]*))?([\#][^\s\n]*)?\)?/';
 		if (!is_string($url))
 			throw new Exception("Argument 'url' is invalid. Not a string.");
 		if (!(preg_match($urlPattern, $url)))
 			throw new Exception("Argument 'url' is invalid. Not a URL.");
 		$this->url = $url;
-		
+
 		$this->fetchResource();
+
+		$this->fetchData();
+
+	}
+
+/*===========================================================================*/
+// PUBLIC METHODS
+/*===========================================================================*/
+	/**
+	 * 
+	 * @return string
+	 */
+	public function getContentType() {
+		return $this->contentType;
+	}
+
+	/**
+	 * 
+	 * @return array
+	 */
+	public function getAllData() {
+		return $this->data;
+	}
+
+	/**
+	 * 
+	 * @return string
+	 */
+	public function getContent() {
+		return $this->content;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getHttpCode() {
+		return $this->httpCode;
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getMeta() {
+		return $this->data['meta'];
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getOgp() {
+		return $this->data['ogp'];
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getTwitter() {
+		return $this->data['twitter'];
+	}
+
+/*===========================================================================*/
+// PRIVATE METHODS
+/*===========================================================================*/
+	private function fetchResource() {
+		$ch = curl_init();
+		curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (compatible; SimpleScraper)');
+		curl_setopt($ch, CURLOPT_URL, $this->url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30); // times out after 30s
+		$this->content = curl_exec($ch);
+		$info = curl_getinfo($ch);
+		curl_close($ch);
+
+		$this->httpCode = $info['http_code'];
+		$this->contentType = $info['content_type'];
+
+		if (((int) $this->httpCode) >= 400) {
+			throw new Exception('STATUS CODE: ' . $this->httpCode);
+		}
+	}
+
+/*===========================================================================*/
+// PROTECTED METHODS
+/*===========================================================================*/
+	protected function fetchData() {
 		libxml_use_internal_errors(true);
 		$dom = new DOMDocument(null, 'UTF-8');
 		$dom->loadHTML($this->content);
@@ -56,9 +147,9 @@ class SimpleScraper {
 			$attributes = $metaTags->item($i)->attributes;
 			$attrArray = array();
 			foreach ($attributes as $attr) $attrArray[$attr->nodeName] = $attr->nodeValue;
-			
+
 			if (
-				array_key_exists('property', $attrArray) && 
+				array_key_exists('property', $attrArray) &&
 				preg_match('~og:([a-zA-Z:_]+)~', $attrArray['property'], $matches)
 			) {
 				$this->data['ogp'][$matches[1]] = $attrArray['content'];
@@ -74,88 +165,5 @@ class SimpleScraper {
 				$this->data['meta'][$attrArray['name']] = $attrArray['content'];
 			}
 		}
-		
 	}
-	
-/*===========================================================================*/
-// PUBLIC METHODS
-/*===========================================================================*/
-	/**
-	 * 
-	 * @return string
-	 */
-	public function getContentType() {
-		return $this->contentType;
-	}
-	
-	/**
-	 * 
-	 * @return array
-	 */
-	public function getAllData() {
-		return $this->data;
-	}
-	
-	/**
-	 * 
-	 * @return string
-	 */
-	public function getContent() {
-		return $this->content;
-	}
-	
-	/**
-	 *
-	 * @return string
-	 */
-	public function getHttpCode() {
-		return $this->httpCode;
-	}
-	
-	/**
-	 *
-	 * @return array
-	 */
-	public function getMeta() {
-		return $this->data['meta'];
-	}
-	
-	/**
-	 *
-	 * @return array
-	 */
-	public function getOgp() {
-		return $this->data['ogp'];
-	}
-	
-	/**
-	 *
-	 * @return array
-	 */
-	public function getTwitter() {
-		return $this->data['twitter'];
-	}
-	
-/*===========================================================================*/
-// PRIVATE METHODS
-/*===========================================================================*/
-	private function fetchResource() {
-		$ch = curl_init();
-		curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (compatible; SimpleScraper)');
-		curl_setopt($ch, CURLOPT_URL, $this->url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 30); // times out after 30s
-		$this->content = curl_exec($ch);
-		$info = curl_getinfo($ch);
-		curl_close($ch);
-		
-		$this->httpCode = $info['http_code'];
-		$this->contentType = $info['content_type'];
-		
-		if (((int) $this->httpCode) >= 400) {
-			throw new Exception('STATUS CODE: ' . $this->httpCode);
-		}
-	}	
 }
